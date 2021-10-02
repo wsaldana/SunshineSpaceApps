4304902ed083e87c83c13fc61a48e048ce48eba7
Raúl Angel Jiménez <raul.jimenez@ext.firstlight.ai>
2021-10-02 13:15:27 -0600

    Feat: start drawer menu

--- App.js
+++ App.js
@@ -1,13 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import './app.css'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 
+
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>UVGSunshine</Text>
+    </View>
+  )
+}
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <div class="container-drawer">
+        <div class="parameters-drawer">
+          <Text style={{ width: '100%' }}>Parameters</Text>
+        </div>
+      </div>
+      {/* <DrawerItem
+        label="Close drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
+      <DrawerItem
+        label="Toggle drawer"
+        onPress={() => props.navigation.toggleDrawer()}
+      /> */}
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="UVGSunshine" component={Feed} />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your Heo!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
