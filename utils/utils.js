0987e8c0e5bc94ff2ccc4a629f177f7968ef28e0
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-10-03 19:53:13 -0600

    Fixed utils module

--- utils/utils.js
+++ utils/utils.js
@@ -14,43 +14,48 @@ const groupByTimeResolution = (data, time_resolution) => {
         const day = date.slice(6, 8);
         var count = 0;
 
-        // Split data by year
-        if(labels.includes(year)){
-            values[count].append(data.date)
-        }else{
-            labels.append(year);
-            values.append([data.date]);
-            count++;
-        }
-
-        if(time_resolution == 'SEMESTER'){
-            temp_labels = [];
-            temp_values = [];
-            for(var i = 0; i < labels.length; i++){
-                if(values.length>6){
-                    temp_values.append(values[i].slice(0, 6));
-                    temp_values.append(values[i].slice(6, values[i].length));
-                    temp_labels.append(labels[i]+" S1");
-                    temp_labels.append(labels[i]+" S2");
-                }else {
-                    temp_labels.append(labels[i]+" S1");
-                    temp_values.append(values[i].slice(0, values[i].length));
-                }
+        if(time_resolution == 'MONTH'){
+            if(labels.includes(year+"/"+month)){
+                values[values.length -1].push(data[date])
+            }else{
+                labels.push(year+"/"+month);
+                values.push([data[date]]);
             }
-        }else if(time_resolution == 'TRIMESTER'){
-            // TODO
-        }else if(time_resolution == 'MONTH'){
-            // TODO
         }else{
-            return {
-                "labels": [], 
-                "values": []
+            // Split data by year
+            if(labels.includes(year)){
+                values[values.length -1].push(data[date])
+            }else{
+                labels.push(year);
+                values.push([data[date]]);
+            }
+
+            if(time_resolution == 'SEMESTER'){
+                temp_labels = [];
+                temp_values = [];
+                for(var i = 0; i < labels.length; i++){
+                    if(values.length>6){
+                        temp_values.push(values[i].slice(0, 6));
+                        temp_values.push(values[i].slice(6, values[i].length));
+                        temp_labels.push(labels[i]+" S1");
+                        temp_labels.push(labels[i]+" S2");
+                    }else {
+                        temp_labels.push(labels[i]+" S1");
+                        temp_values.push(values[i].slice(0, values[i].length));
+                    }
+                }
+                labels = temp_labels;
+                values = temp_values;
+            }else if(time_resolution == 'TRIMESTER'){
+                // TODO
+            }else{
+                // TODO
             }
         }
     })
 
     // Reduce grouped data to period average
-    values.map((item) => {
+    values = values.map((item) => {
         var sum = 0;
         for(var i = 0; i <item.length; i++){
             sum += item[i];
